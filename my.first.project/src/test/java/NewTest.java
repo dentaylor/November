4a5688cf9863a7cdf8c44d1ef5542878939402a6
Dennis Taylor <dentaylor@hotmail.com>
2022-11-02 18:53:50 -0500

    Changes project compliance to 11

---
+++ my.first.project/src/test/java/NewTest.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void canRunTestNgTest() {
+	  // If this doesn't pass then nothing will
+  }
+  
+  @Test
+  public void canUseVar() {
+	  String myString = "Dennis";
+	  var myVarString = "Dennis";			  
+  }
+}
