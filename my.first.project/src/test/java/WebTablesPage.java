a839809fb0211a3d091292a2855abdc20d93490b
abel <abel@localhost>
2022-12-05 18:35:44 -0600

    Merge branch 'control-extensions' of https://github.com/dentaylor/November.git into control-extensions

--- my.first.project/src/test/java/WebTablesPage.java
+++ my.first.project/src/test/java/WebTablesPage.java
@@ -3,20 +3,24 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
-public class WebTablesPage extends PageObject{
-	
-	@FindBy(how=How.XPATH, using="//DIV[@class='rt-tr-group'][2]")
-	private WebElement tableRowElement;
-	
-	private String URL = "https://demoqa.com/webtables";
-	
-	public WebTablesPage(WebDriver driver) {
+import ControlExtensions.Angular.Table;
+
+public class webtablesPage extends PageObject {
+
+	@FindBy(how = How.CLASS_NAME, using = "rt-table")
+	private WebElement webTable;
+
+	public webtablesPage(WebDriver driver) {
 		super(driver);
 	}
-	
-	public TableCell getCell(int columnIndex) {
-		this.driver.navigate().to(URL);
-		return new TableRow(tableRowElement).getCell(columnIndex);
-		
+
+	public webtablesPage navigate() {
+		this.driver.get("https://demoqa.com/webtables");
+		return this;
 	}
+
+	public Table getTable() {
+		return new Table(webTable);
+	}
+
 }
